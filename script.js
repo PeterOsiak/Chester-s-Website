f016ece959b276c9eec3b47e25f806bc80f965c1
Piotr Osiak <39980918+PeterOsiak@users.noreply.github.com>
2019-01-14 00:07:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,24 @@ window.onscroll = function() {
 }
 
 function scrollFunction() {
-    if  (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
-        document.querySelector('#scroll-btn').style.display = "block";
+    if  (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+        document.querySelector('#scroll-btn').style.display = 'block';
     } else {
-       document.querySelector('#scroll-btn').style.display = "none"; 
+        document.querySelector('#scroll-btn').style.display = 'none'; 
     }
 }
+
+// HEADER DISSAPERING BUTTONS
+
+//
+//window.onscrollTwo = function() {
+//    scrollFunction()
+////}
+//
+//function scrollFunctionTwo() {
+//    if  (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+//        document.getElementsByClassName('btn-header').style.display = "inline-blocksss";
+//    } else {
+//       document.getElementsByClassName('btn-header').style.display = "none"; 
+//    }
+//}
