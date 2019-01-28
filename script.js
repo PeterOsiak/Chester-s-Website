7f611f5e5c36f12c076ef26dbe4932e04bff45ad
Piotr Osiak <39980918+PeterOsiak@users.noreply.github.com>
2019-01-28 16:25:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,21 @@
 FIXED SCROLL BUTTON
 **********************/
 
-window.onscroll = function() {
-    scrollFunction()
-};
+
 
 function scrollFunction() {
-    if  (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+    if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
         document.querySelector('#scroll-btn').style.display = 'block';
-        console.log("I'm ON");
+        
     } else {
-        document.querySelector('#scroll-btn').style.display = 'none'; 
-        console.log("I'm OFF");
+        document.querySelector('#scroll-btn').style.display = 'none';
+      
     }
-};
+}
 
+window.onscroll = function () {
+    scrollFunction();
+};
 
 /**********************
 MOBILE NAVIGATION (HAMBURGER MENU)
@@ -23,47 +24,72 @@ MOBILE NAVIGATION (HAMBURGER MENU)
 
 //klikam w hamburgera, to zamienia się w X i odwrotnie
 
-document.querySelector('.icon').addEventListener('click', classToggle);
-
 function classToggle() {
     this.classList.toggle('ion-ios-menu');
     this.classList.toggle('ion-ios-close');
 }
 
-//
-//
-//document.querySelector('.mobile-nav-icon').onclick = function() {
-//    hamburgerFunction();
-//}
+document.querySelector('.mobile-nav-icon').addEventListener('click', classToggle);
 
-document.querySelector('.icon').addEventListener('click', hamburgerFunction)
+var btn = document.getElementsByClassName('icon');
 
-/* Jezeli klikam w ikonę, KIEDY JEST HAMBURGEREM, zmenia się w X i pojawia się rozwijane menu */
+
 function hamburgerFunction() {
-    //jeżeli klikam w hamburgera to zamienia się w X oraz wysuwa się menu
-    var x = 'none';
-        if(document.querySelector('.icon').hasClass = '.hamburger') {
-             document.querySelector('.js--main-nav').style.display = 'block';
-        } else {
-             document.querySelector('.js--main-nav').style.display = 'none';
-        }
-    
+  
+    if (btn.classList.contains('ion-ios-close')) {
+        document.querySelector('.js--main-nav').style.display = 'block';
+    } else {
+        document.querySelector('.js--main-nav').style.display = 'none';
+    }
 }
 
+btn.addEventListener('click', hamburgerFunction);
+
+
+
+
+
 //
-///* Jeżeli klikamy w ikonę, KIEDY JEST X, pojawia się hamburger i zwija się menu */
+//document.querySelector('.icon').addEventListener('click', hamburgerFunction);
 //
-//document.getElementsByClassName("ion-ios-close").onclick = function() {
-//    closeFunction();
-//}
 //
-//function closeFunction() {
+////
+////
+////document.querySelector('.mobile-nav-icon').onclick = function() {
+////    hamburgerFunction();
+////}
+//
+////document.querySelector('.icon').addEventListener('click', hamburgerFunction)
+//
+///* Jezeli klikam w ikonę, KIEDY JEST HAMBURGEREM, zmenia się w X i pojawia się rozwijane menu */
+//function hamburgerFunction() {
 //    //jeżeli klikam w hamburgera to zamienia się w X oraz wysuwa się menu
-//    document.querySelector('.ion-ios-close').addEventListener('click', function() {
-//        if(document.querySelector('.icon').hasClass = '.ion-ios-close') {
-//             document.querySelector('.js--main-nav').style.display = 'none';
-//             document.querySelector('.icon').classList.remove('ion-ios-close');
-//             document.querySelector('.icon').classList.add('ion-ios-menu');
-//        } 
-//    })
+//    var element = document.getElementById('hamburger');
+//    
+//    if(element.classList.contains('ion-ios-close')) {
+//            document.querySelector('.js--main-nav').style.display = 'block';
+//            console.log("Menu down");
+//        } else {
+//            document.querySelector('.js--main-nav').style.display = 'none';
+//            console.log("Menu up");
+//        }
+//    
 //}
+//
+////
+/////* Jeżeli klikamy w ikonę, KIEDY JEST X, pojawia się hamburger i zwija się menu */
+////
+////document.getElementsByClassName("ion-ios-close").onclick = function() {
+////    closeFunction();
+////}
+////
+////function closeFunction() {
+////    //jeżeli klikam w hamburgera to zamienia się w X oraz wysuwa się menu
+////    document.querySelector('.ion-ios-close').addEventListener('click', function() {
+////        if(document.querySelector('.icon').hasClass = '.ion-ios-close') {
+////             document.querySelector('.js--main-nav').style.display = 'none';
+////             document.querySelector('.icon').classList.remove('ion-ios-close');
+////             document.querySelector('.icon').classList.add('ion-ios-menu');
+////        } 
+////    })
+////}
